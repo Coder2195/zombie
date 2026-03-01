48cf9281bc088110b55c4923f076ce60f5933a21
Coder2195 <coder2195mail@gmail.com>
2026-03-01 09:59:25 -0500

    adjustments

--- src/globals/data_tracker.gd
+++ src/globals/data_tracker.gd
@@ -2,6 +2,13 @@ class_name GameDataTracker extends Node2D;
 
 var current_wave = 1;
 var zombies_killed = 0;
+var time = 0.0;
+
+func start_time():
+  time = Time.get_ticks_msec();
+
+func stop_time():
+  return Time.get_ticks_msec() - time;
 
 func reset():
     current_wave = 1;
